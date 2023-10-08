3465850778d37973ac582f823b343d74203b4c03
Aftermath001 <alvinasiachi@gmail.com>
2023-10-08 23:16:30 +0300

    Hero-section

--- my-app/src/Pages/Home/HeroSection.jsx
+++ my-app/src/Pages/Home/HeroSection.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 function HeroSection() {
   return (
-    <div><h1>HEROES</h1></div>
+    <section id ="heroSection" className='hero-section'>
+        <div className='hero-section-content-box'>
+            <div className='hero-section-content'>
+                <p className='section-title'>Hello, I'm Adams</p>
+                <h1 className='hero-section-title'>
+                    <span className='hero-section-title-color'>Full Stack</span>{" "} <br />
+                    Developer
+                </h1>
+                <p className='hero-section-description'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    <br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quam perferendis quisquam! Aliquam, consequuntur.
+                </p>
+            </div>
+            <button className='btn btn-primary'>Get In Touch</button>
+        </div>
+        <div className='hero-secttion-image'>
+            <img src='./images/Bg.png' alt='Hero Section'/>
+        </div>
+    </section>
   )
 }
 
